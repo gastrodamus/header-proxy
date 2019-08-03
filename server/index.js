c5260b3c3d3bdc14899eeea10ec3d07f4cfad733
esme <23175119+esme@users.noreply.github.com>
2019-08-03 15:07:48 -0700

    initial setup for header proxy

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
+const newrelic = require('newrelic');
 const express = require ('express');
-const PORT = 5001;
+const PORT = 3000;
 const app = express();
-const path = require('path'); 
+const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const httpProxy = require('http-proxy');
-const proxy = httpProxy.createProxyServer();
+const axios = require('axios');
 
 app.use(morgan('dev'));
 app.use('/:id', express.static(path.join(__dirname, '/../client')));
@@ -13,26 +13,27 @@ app.use('/:id', express.static(path.join(__dirname, '/../client')));
 const gallery = 'http://localhost:3000';
 const reservation = 'http://localhost:3001';
 const popular = 'http://localhost:3002';
-const header = 'http://localhost:3003';
+const header = 'http://localhost:3003/api/header';
 
-app.all('/gallery/:id', function(req, res) {
-    console.log('redirecting to gallery');
-    proxy.web(req, res, { target: gallery });
-});
+// app.all('/gallery/:id', function(req, res) {
+//     console.log('redirecting to gallery');
+//     proxy.web(req, res, { target: gallery });
+// });
 
-app.all('/reservation/:id', function(req, res) {
-    console.log('redirecting to reservation');
-    proxy.web(req, res, { target: reservation });
-});
+// app.all('/reservation/:id', function(req, res) {
+//     console.log('redirecting to reservation');
+//     proxy.web(req, res, { target: reservation });
+// });
 
-app.all('/popular/:id', function(req, res) {
-    console.log('redirecting to popular');
-    proxy.web(req, res, { target: popular });
-});
+// app.all('/popular/:id', function(req, res) {
+//     console.log('redirecting to popular');
+//     proxy.web(req, res, { target: popular });
+// });
 
-app.all('/header/:id', function(req, res) {
-    console.log('redirecting to header');
-    proxy.web(req, res, { target: header });
+app.get('/header/:id', function(req, res) {
+  console.log(header + '/' + req.params.id)
+  axios.get(header + '/' + req.params.id)
+    .then(({data}) => res.send(data))
 });
 
 app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`))
---
+++ server/script.js
@@ -0,0 +1,17 @@
+/* eslint-disable prefer-const */
+/* eslint-disable import/no-mutable-exports */
+/* eslint-disable no-restricted-syntax */
+import http from 'k6/http';
+import { check, sleep } from "k6";
+import faker from 'cdnjs.cloudflare.com/ajax/libs/Faker/3.1.0/faker.min.js';
+
+export let options = {
+  rps: 1000,
+  vus: 150,
+  duration: '45s',
+};
+
+export default function () {
+  let id = faker.random.number({ min: 1, max: 1000 });
+  http.get(`http://localhost:3000/header/${id}`);
+}
